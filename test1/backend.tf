29ba184fa0c580d0ce06bb5cf53914b249477b5d
siakan1107 <121685349+siakan1107@users.noreply.github.com>
2023-03-31 11:19:28 +0900

    Delete backend.tf

--- test1/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform{
-  backend "s3" {
-     bucket = "kibeom-test-terraform"
-     key = "terraform_test/chapter6/terraform.tfstate"
-     region = "ap-northeast-2"
-     encrypt = true
-     dynamodb_table = "terraform-lock"
- }
-}
